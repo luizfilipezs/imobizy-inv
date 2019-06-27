0ca4fa24ae05bcec01efaf5dd7d964e34310f167
Luiz Filipe da Silva <luizfilipe.tj@icloud.com>
2019-06-27 10:33:34 -0300

    Create login component and update routes and main styles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { LoginComponent } from './login/login.component';
+//import { DashboardComponent } from './dashboard/dashboard.component';
+
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  }
+  /*
+  {
+    path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    component: DashboardComponent
+  }
+  */
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
